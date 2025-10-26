17ffc8101f515e5460301e681971c119c934b1c1
ydking0911 <ydking0911@gmail.com>
2025-10-26 18:38:50 +0900

    chore(view): remove unused Scanner code
    - remove unnecessary Scanner import and instance
    - clean up unused code for better maintainability

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,11 +1,8 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
-import java.util.Scanner;
 
 public class InputView {
-
-    private static final Scanner scanner = new Scanner(System.in);
 
     public static String readCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분됨");
